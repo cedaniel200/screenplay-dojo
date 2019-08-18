793495dc71496c35171522d6093ef3184dbbfc95
cesar daniel <cdanielmg200@gmail.com>
2019-08-18 17:58:02 -0500

    Se agrega la base del proyecto en java con sus respectivos comentarios

---
+++ src/main/java/Pasos.java
@@ -0,0 +1,41 @@
+import com.sun.xml.internal.bind.v2.TODO;
+
+public class Pasos {
+    // TODO 10 Crear el paquete base de la aplicación : grupo "com.cedaniel200.screenplay" + nombre-app "dojo" = com.cedaniel200.screenplay.dojo
+
+    // TODO 20 Crear el paquetes userinterface dentro del paquete base
+    // TODO 21 Crear el page InicioSesionPage.java dentro del paquete userinterface, heredando de PageObject
+    // TODO 22 Agregar la anotacion de clase @DefaultUrl("https://github.com/login")
+
+    // TODO 24 Crear el paquetes task dentro del paquete base
+    // TODO 25 Crear el paquetes model dentro del paquete base
+    // TODO 26 Crear el modelo Usuario.java dentro del paquete model, con:
+    //  los atributos usuario, contrasena y métodos get
+    // TODO 27 Crear la tarea dentro del paquete task, implementando la interfaz Task
+    // TODO 28 Crear la instancia de la tarea creada usando el método instrumented
+    // TODO 29 Mapear lo elementos de la pagina de inicio de sesion de GitHub en InicioSesionPage
+    // TODO 30 Implementar el método performAs de la tarea usando los elementos mapeados
+
+    // TODO 33 Crear UsuarioBuilder.java dentro del paquete model
+
+    // TODO 34 Validar el estado del modelo usuario verificando que los string no estan vacios o nulos
+    // TODO 35 Crear el paquetes exception dentro del paquete base
+    // TODO 36 Crear la excepción UsuarioEstadoIlegalException.java dentro del paquete exception
+
+    // TODO 37 Crear el paquetes util dentro del paquete base
+    // TODO 38 Crear la clase ValidadorString.java y mover el método que
+
+    // TODO 40 Crear el paquetes question dentro del paquete base
+    // TODO 41 Crear la question
+    // TODO 42 Crear InicioPage.java dentro del paquere userinterface
+    // TODO 43 Mapear el elemento con el que se verificara la visibilidad de la pagina de inicio
+
+
+    // TODO 47 Revisar la documentación
+    // TODO 48 Agregar @Step("") o @Subject("") para mejorar la salida del reporte
+
+    // TODO 49 Configurar las variables -Dusuario= y -Dcontrasena= para no tener esos datos en el código
+    // TODO 50 Configurar las variabes -Dcontext= -Dwebdriver.driver= para configurar el contexto y el driver a usar
+
+    // TODO 53 Crear la excepción AutenticacionFallidaError.java dentro del paquete exception
+}
---
+++ src/test/java/Pasos.java
@@ -0,0 +1,44 @@
+public class Pasos {
+    // TODO 9 Crear el paquete base de la aplicación : grupo "com.cedaniel200.screenplay" + nombre-app "dojo" = com.cedaniel200.screenplay.dojo
+    // TODO 13 Crear el paquete runner dentro del paquede base
+    // TODO 14 Crear el runner (InicioSesion.java) utilizando las siguientes anotaciones de clase:
+    /*
+      @RunWith(CucumberWithSerenity.class)
+      @CucumberOptions(features = "src/test/resources/features/inicio_sesion.feature",
+      glue = {"com.cedaniel200.screenplay.dojo.stepdefinition"}, snippets = SnippetType.CAMELCASE)
+     */
+
+
+    // TODO 15 crear el paquete steddefinition dentro del paquete base
+    // TODO 16 Ejecutar el runner
+    // TODO 17 Crear la definicion de los pasos (InicioSesionStepDefinition.java) utilizando el resultado de la ejecución del runner
+    // TODO 18 Crear en InicioSesionStepDefinition el método setTheStage colocandole la anotación de método
+    //  @Before
+    //  y la siguiente linea de código
+    //  OnStage.setTheStage(new OnlineCast());
+
+    // TODO 19 Implemar el médoto con la anotación @Given donde se debe:
+    //  Crear el actor
+    //  abrir la pagina web haciendo uso del page InicioSesionPage
+
+    // TODO 23 Implemar el médoto con la anotación @When donde se debe:
+    //  Crear la tarea la cual debe recibir un usuario
+
+    // TODO 31 Ejecutar la prueba
+    // TODO 32 Mejorar la legibilidad usando un Builder
+
+    // TODO 39 Implemar el médoto con la anotación @Then donde se debe:
+    //  verificar si es visible la pagina de inicio
+
+    // TODO 44 Limpiar la ejecuciones anteriores de la prueba(Ejecutar tarea clean)
+    // TODO 45 Ejecutar la prueba (Ejecutar la tarea test)
+    // TODO 46 Generar la documentación (Ejecutar la tarea aggregate)
+
+    // TODO 52 Usar el método orComplainWith dentro de la implementacion del método con la anotacion @Then
+
+    // TODO 53 Limpiar la ejecuciones anteriores de la prueba(Ejecutar tarea clean)
+    // TODO 54 Ejecutar la prueba (Ejecutar la tarea test)
+    //  con datos validos y luego con datos invalidos
+    // TODO 55 Generar la documentación (Ejecutar la tarea aggregate)
+
+}
