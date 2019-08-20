3f46ebfa58502983cf53d3cbcc8dc9fd07469392
cesar daniel <cdanielmg200@gmail.com>
2019-08-20 15:02:06 -0500

    Se mapean los elementos web de la pagina de inicio de sesion

--- src/main/java/com/cedaniel200/screenplay/dojo/userinterface/InicioSesionPage.java
+++ src/main/java/com/cedaniel200/screenplay/dojo/userinterface/InicioSesionPage.java
@@ -1,8 +1,16 @@
 package com.cedaniel200.screenplay.dojo.userinterface;
 
 import net.serenitybdd.core.pages.PageObject;
+import net.serenitybdd.screenplay.targets.Target;
 import net.thucydides.core.annotations.DefaultUrl;
+import org.openqa.selenium.By;
 
 @DefaultUrl("https://github.com/login")
 public class InicioSesionPage extends PageObject {
+
+    public static final Target NOMBRE_USUARIO_O_CORREO = Target.the("Username or email field")
+            .located(By.id("login_field"));
+    public static final Target CONTRASENA = Target.the("Password field").located(By.id("password"));
+    public static final Target INGRESAR = Target.the("Sign in").located(By.name("commit"));
+
 }
