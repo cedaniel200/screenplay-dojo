b374c6c4319ae1f0e7bdbdced166f10a96919ef8
cesar daniel <cdanielmg200@gmail.com>
2019-08-20 15:39:30 -0500

    Creacion de UsuarioEstadoIlegalException

---
+++ src/main/java/com/cedaniel200/screenplay/dojo/exception/UsuarioEstadoIlegalException.java
@@ -0,0 +1,8 @@
+package com.cedaniel200.screenplay.dojo.exception;
+
+public class UsuarioEstadoIlegalException extends IllegalStateException {
+
+    public UsuarioEstadoIlegalException(String mensaje) {
+        super(mensaje);
+    }
+}
